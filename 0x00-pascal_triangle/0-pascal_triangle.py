edc580fa990650bbb4162e084168deec3daf312d
kmishmael <kmishmael@gmail.com>
2024-01-04 14:39:37 +0300

    chore: Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""generate a pascal triangle"""
+
+
+def pascal_triangle(n):
+    """generate pascal triangle
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
