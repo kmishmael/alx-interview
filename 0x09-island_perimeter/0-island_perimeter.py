a32e2a9c1352639d215fdaf5d565d0f7f890e478
kmishmael <kmishmael@gmail.com>
2024-03-07 16:16:43 +0300

    chore: island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+"""island perimater"""
+
+def island_perimeter(grid):
+    """island perimeter"""
+    perimeter = 0
+    for i in range(1, len(grid)):
+        for j in range(1, len(grid[i])):
+            current = grid[i][j]
+            if current == 1:
+                # check the top
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+    return perimeter
